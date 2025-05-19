63c766269e2b8eed672c526c3d4c280a30089eb0
NCasella <nicolascasella02@gmail.com>
2025-05-19 12:50:05 -0300

    added seeds functions

---
+++ src/include/seeds.h
@@ -0,0 +1,12 @@
+#ifndef SEEDS_H
+#define SEEDS_H
+
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+void setSeed(int64_t s);
+uint8_t nextChar(void);
+
+#endif /* SEEDS_H */
---
+++ src/seeds.c
@@ -0,0 +1,14 @@
+#include "seeds.h"
+
+int seed;
+
+void
+setSeed(int64_t s)
+{
+seed = (s ^ 0x5DEECE66DL) & ((1LL << 48) - 1);
+}
+uint8_t
+nextChar(void){
+seed = (seed * 0x5DEECE66DL + 0xBL) & ((1LL << 48) - 1);
+return (uint8_t)(seed>>40);
+}
